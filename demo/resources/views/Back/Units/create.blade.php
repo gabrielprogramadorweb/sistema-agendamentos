e985f661a0664bc66ca9a73ed2a282ce7f59c5d7
gabrielprogramadorweb <gabriel.programador.ads@gmail.com>
2024-04-22 19:43:17 -0300

    Ajuste realizado em forms

--- demo/resources/views/Back/Units/create.blade.php
+++ demo/resources/views/Back/Units/create.blade.php
@@ -19,89 +19,92 @@
 
         <form action="{{ route('units.store') }}" method="POST">
             @csrf  {{-- CSRF Token is essential for form security --}}
+            <div class="row">
+                <div class="form-group col-md-3">
+                    <label for="name">Nome:</label>
+                    <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
+                    @error('name')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
 
-            <div class="form-group">
-                <label for="name">Nome:</label>
-                <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
-                @error('name')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
+                <div class="form-group col-md-3">
+                    <label for="email">E-mail:</label>
+                    <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
+                    @error('email')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="password">Senha:</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                    @error('password')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="phone">Telefone:</label>
+                    <input type="text" class="form-control" id="phone" name="phone" value="{{ old('phone') }}">
+                    @error('phone')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="coordinator">Coordenador(a):</label>
+                    <input type="text" class="form-control" id="coordinator" name="coordinator" value="{{ old('coordinator') }}">
+                    @error('coordinator')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="address">Endereço:</label>
+                    <input type="text" class="form-control" id="address" name="address" value="{{ old('address') }}">
+                    @error('address')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+
+                <div class="form-group col-md-3">
+                    <label for="starttime">Início:</label>
+                    <input type="time" class="form-control" id="starttime" name="starttime" value="{{ old('starttime') }}">
+                    @error('starttime')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="endtime">Fim:</label>
+                    <input type="time" class="form-control" id="endtime" name="endtime" value="{{ old('endtime') }}">
+                    @error('endtime')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="servicetime">Tempo de serviço:</label>
+                    <input type="number" class="form-control" id="servicetime" name="servicetime" value="{{ old('servicetime') }}">
+                    @error('address')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="active">Ativo:</label>
+                    <input type="checkbox" id="active" name="active" {{ old('active') ? 'checked' : '' }}>
+                    @error('active')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="form-group col-md-10">
+                    <button type="submit" class="btn btn-primary">Criar</button>
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="email">E-mail:</label>
-                <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
-                @error('email')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <label for="password">Senha:</label>
-                <input type="password" class="form-control" id="password" name="password" required>
-                @error('password')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <label for="phone">Telefone:</label>
-                <input type="text" class="form-control" id="phone" name="phone" value="{{ old('phone') }}">
-                @error('phone')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <label for="coordinator">Coordenador(a):</label>
-                <input type="text" class="form-control" id="coordinator" name="coordinator" value="{{ old('coordinator') }}">
-                @error('coordinator')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <label for="address">Endereço:</label>
-                <input type="text" class="form-control" id="address" name="address" value="{{ old('address') }}">
-                @error('address')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-
-
-            <div class="form-group">
-                <label for="starttime">Início:</label>
-                <input type="time" class="form-control" id="starttime" name="starttime" value="{{ old('starttime') }}">
-                @error('starttime')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <label for="endtime">Fim:</label>
-                <input type="time" class="form-control" id="endtime" name="endtime" value="{{ old('endtime') }}">
-                @error('endtime')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <label for="servicetime">Tempo de serviço:</label>
-                <input type="number" class="form-control" id="servicetime" name="servicetime" value="{{ old('servicetime') }}">
-                @error('address')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <label for="active">Ativo:</label>
-                <input type="checkbox" id="active" name="active" {{ old('active') ? 'checked' : '' }}>
-                @error('active')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <button type="submit" class="btn btn-primary">Criar</button>
         </form>
     </div>
 @endsection
--- demo/resources/views/Back/Units/edit.blade.php
+++ demo/resources/views/Back/Units/edit.blade.php
@@ -19,52 +19,57 @@
         <form action="{{ route('units.update', $unit->id) }}" method="POST">
             @csrf
             @method('PUT')
-            <div class="form-group">
-                <label for="name">Nome:</label>
-                <input type="text" class="form-control" id="name" name="name" value="{{ old('name', $unit->name) }}" required>
+            <div class="row">
+                <div class="form-group col-md-3">
+                    <label for="name">Nome:</label>
+                    <input type="text" class="form-control" id="name" name="name" value="{{ old('name', $unit->name) }}" required>
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="email">E-mail:</label>
+                    <input type="email" class="form-control" id="email" name="email" value="{{ old('email', $unit->email) }}" required>
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="phone">Telefone:</label>
+                    <input type="text" class="form-control" id="phone" name="phone" value="{{ old('phone', $unit->phone) }}">
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="coordinator">Coordenador(a):</label>
+                    <input type="text" class="form-control" id="coordinator" name="coordinator" value="{{ old('coordinator', $unit->coordinator) }}">
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="address">Endereço:</label>
+                    <input type="text" class="form-control" id="address" name="address" value="{{ old('address', $unit->address) }}">
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="starttime">Início:</label>
+                    <input type="time" class="form-control" id="starttime" name="starttime" value="{{ old('starttime', $unit->starttime) }}">
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="endtime">Fim:</label>
+                    <input type="time" class="form-control" id="endtime" name="endtime" value="{{ old('endtime', $unit->endtime) }}">
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="servicetime">Tempo de serviço:</label>
+                    <input type="number" class="form-control" id="servicetime" name="servicetime" value="{{ old('servicetime', $unit->servicetime) }}">
+                </div>
+
+                <div class="form-group col-md-3">
+                    <label for="active">Ativo:</label>
+                    <input type="checkbox" id="active" name="active" {{ $unit->active ? 'checked' : '' }}>
+                </div>
+                <div class="form-group col-md-10">
+                    <button type="submit" class="btn btn-primary">Atualizar</button>
+                </div>
+
             </div>
 
-            <div class="form-group">
-                <label for="email">E-mail:</label>
-                <input type="email" class="form-control" id="email" name="email" value="{{ old('email', $unit->email) }}" required>
-            </div>
-
-            <div class="form-group">
-                <label for="phone">Telefone:</label>
-                <input type="text" class="form-control" id="phone" name="phone" value="{{ old('phone', $unit->phone) }}">
-            </div>
-
-            <div class="form-group">
-                <label for="coordinator">Coordenador(a):</label>
-                <input type="text" class="form-control" id="coordinator" name="coordinator" value="{{ old('coordinator', $unit->coordinator) }}">
-            </div>
-
-            <div class="form-group">
-                <label for="address">Endereço:</label>
-                <input type="text" class="form-control" id="address" name="address" value="{{ old('address', $unit->address) }}">
-            </div>
-
-            <div class="form-group">
-                <label for="starttime">Início:</label>
-                <input type="time" class="form-control" id="starttime" name="starttime" value="{{ old('starttime', $unit->starttime) }}">
-            </div>
-
-            <div class="form-group">
-                <label for="endtime">Fim:</label>
-                <input type="time" class="form-control" id="endtime" name="endtime" value="{{ old('endtime', $unit->endtime) }}">
-            </div>
-
-            <div class="form-group">
-                <label for="servicetime">Tempo de serviço:</label>
-                <input type="number" class="form-control" id="servicetime" name="servicetime" value="{{ old('servicetime', $unit->servicetime) }}">
-            </div>
-
-            <div class="form-group">
-                <label for="active">Ativo:</label>
-                <input type="checkbox" id="active" name="active" {{ $unit->active ? 'checked' : '' }}>
-            </div>
-
-            <button type="submit" class="btn btn-primary">Update</button>
         </form>
     </div>
 @endsection
