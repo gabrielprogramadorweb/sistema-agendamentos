9dabee41ce8c4464040328cfc9ff1aa03529551f
gabrielprogramadorweb <gabriel.programador.ads@gmail.com>
2024-04-24 00:42:31 -0300

    Ajustes

--- agendamentos/resources/js/app.js
+++ agendamentos/resources/js/app.js
@@ -5,7 +5,6 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
-import SuccessModal from './Components/SuccessModal.vue'; // Ensure path correctness
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -14,7 +13,6 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         const app = createApp({ render: () => h(App, props) })
-            .component('SuccessModal', SuccessModal) // Registering SuccessModal globally
             .use(plugin)
             .use(ZiggyVue, Ziggy);
 
