f3adc82cbffd82a1c73ea75f36714b7f3a5af021
gabrielprogramadorweb <gabriel.programador.ads@gmail.com>
2024-04-24 21:00:08 -0300

    [master] - Método para marcar todos serviços como ativo implementado.

--- agendamentos/resources/views/Back/Layout/main.blade.php
+++ agendamentos/resources/views/Back/Layout/main.blade.php
@@ -312,6 +312,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 
 @yield('js')
+@yield('scripts')
 @push('scripts')
     <script src="{{ asset('back/vendor/datatables/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('back/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
--- agendamentos/resources/views/Back/Units/services.blade.php
+++ agendamentos/resources/views/Back/Units/services.blade.php
@@ -14,26 +14,32 @@
                 {{ session('info') }}
             </div>
         @endif
-            <div class="card shadow mb-4">
-                <div class="card-header py-3 d-flex justify-content-between align-items-center">
-                    <h6 class="m-0 font-weight-bold text-primary">{{ $title }}</h6>
-                    <a href="{{ route('units.index') }}" class="btn btn-secondary btn-sm">Voltar</a>
-                </div>
-                <div class="card-body">
-                    <form action="{{ route('units.services.store', $unit->id) }}" method="POST">
-                        @csrf
-                        @method('PUT')
-                        <button type="submit" class="btn btn-sm btn-success">Salvar</button><br>
-                        <button type="button" id="btnToogleAll" class="btn btn-sm btn-primary mt-2 mb-1">Marcar todos</button>
-                        {!! $servicesOptions  !!}
-                    </form>
-                </div>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3 d-flex justify-content-between align-items-center">
+                <h6 class="m-0 font-weight-bold text-primary">{{ $title }}</h6>
+                <a href="{{ route('units.index') }}" class="btn btn-secondary btn-sm">Voltar</a>
             </div>
-
+            <div class="card-body">
+                <form action="{{ route('units.services.store', $unit->id) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <button type="submit" class="btn btn-sm btn-success">Salvar</button><br>
+                    <button type="button" id="btnToggleAll" class="btn btn-sm btn-primary mt-2 mb-1">Marcar todos</button>
+                    {!! $servicesOptions !!}
+                </form>
+            </div>
+        </div>
     </div>
 @endsection
 
 @section('scripts')
-
-
+    <script>
+        document.getElementById('btnToggleAll').addEventListener('click', function() {
+            let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+            // Set all checkboxes to checked
+            checkboxes.forEach(function(checkbox) {
+                checkbox.checked = true;
+            });
+        });
+    </script>
 @endsection
