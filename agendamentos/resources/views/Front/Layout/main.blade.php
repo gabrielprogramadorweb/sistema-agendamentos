604a83adc4042abe23dc586897db6a7a80e0b598
gabrielprogramadorweb <gabriel.programador.ads@gmail.com>
2024-05-08 15:28:53 -0300

    [master] - Ajustando barra de navegação - atual

--- agendamentos/resources/views/Front/Layout/main.blade.php
+++ agendamentos/resources/views/Front/Layout/main.blade.php
@@ -93,7 +93,7 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
+                            {{ __('Editar Perfil') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
@@ -103,7 +103,7 @@
                             <x-dropdown-link :href="route('logout')"
                                              onclick="event.preventDefault();
                                       this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('Sair') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
