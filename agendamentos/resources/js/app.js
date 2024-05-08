77ba542de8712a1d1db65e04f63ef8bec7c91d96
gabrielprogramadorweb <gabriel.programador.ads@gmail.com>
2024-05-08 11:13:50 -0300

    [master] - Auth implementado!

--- agendamentos/resources/js/app.js
+++ agendamentos/resources/js/app.js
@@ -1,26 +1,7 @@
 import './bootstrap';
-import '../css/app.css';
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import Alpine from 'alpinejs';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+window.Alpine = Alpine;
 
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, App, props, plugin }) {
-        const app = createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue, Ziggy);
-
-        app.mount(el);
-
-        return app;
-    },
-    progress: {
-        color: '#4B5563',
-    },
-});
+Alpine.start();
--- agendamentos/routes/web.php
+++ agendamentos/routes/web.php
@@ -6,58 +6,68 @@ use App\Http\Controllers\Super\HomeController;
 use App\Http\Controllers\Super\ServiceController;
 use App\Http\Controllers\Super\UnitsController;
 use App\Http\Controllers\Super\UnitsServicesController;
-use App\Http\Controllers\WebHomeController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 //Route::get('/', function () {
-//    return Inertia::render('Welcome', [
-//        'canLogin' => Route::has('login'),
-//        'canRegister' => Route::has('register'),
-//        'laravelVersion' => Application::VERSION,
-//        'phpVersion' => PHP_VERSION,
-//    ]);
+//    return view('welcome');
 //});
-//Route::post('/super/units/{id}/services/store', [UnitsServicesController::class, 'storeServices'])->name('units.services.store');
-Route::get('/', [WebHomeController::class, 'index'])->name('web-home.index');
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/', [\App\Http\Controllers\WebHomeController::class, 'index'])->name('web-home.index')->middleware(['auth', 'verified']);;
+
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->middleware('verified')->name('dashboard');
-
-    Route::prefix('super')->group(function () {
-        Route::get('/', [HomeController::class, 'index'])->name('home.index');
-
-        Route::prefix('units')->group(function () {
-            Route::post('/', [UnitsController::class, 'store'])->name('units.store');
-            Route::get('/', [UnitsController::class, 'index'])->name('units.index');
-            Route::get('/create', [UnitsController::class, 'create'])->name('units.create');
-            Route::delete('/{id}', [UnitsController::class, 'destroy'])->where('id', '[0-9]+')->name('units.destroy');
-            Route::get('/toggle/{id}', [UnitsController::class, 'toggleStatus'])->where('id', '[0-9]+')->name('units.toggleStatus');
-            Route::get('/{id}/edit', [UnitsController::class, 'edit'])->where('id', '[0-9]+')->name('units.edit');
-            Route::put('/{id}', [UnitsController::class, 'update'])->where('id', '[0-9]+')->name('units.update');
-            Route::get('/{id}/services', [UnitsServicesController::class, 'services'])->name('units.services');
-            Route::post('/{id}/services/store', [UnitsServicesController::class, 'storeServices'])->name('units.services.store');
-            Route::put('/{id}/services/save', [UnitsServicesController::class, 'saveServices'])->name('units.services.save');
-        });
-
-        Route::prefix('services')->group(function () {
-            Route::post('/', [ServiceController::class, 'store'])->name('services.store');
-            Route::get('/', [ServiceController::class, 'index'])->name('services.index');
-            Route::get('/create', [ServiceController::class, 'create'])->name('services.create');
-            Route::delete('/{id}', [ServiceController::class, 'destroy'])->where('id', '[0-9]+')->name('services.destroy');
-            Route::get('/toggle/{id}', [ServiceController::class, 'toggleStatus'])->where('id', '[0-9]+')->name('services.toggleStatus');
-            Route::get('/{id}/edit', [ServiceController::class, 'edit'])->where('id', '[0-9]+')->name('services.edit');
-            Route::put('/{id}', [ServiceController::class, 'update'])->where('id', '[0-9]+')->name('services.update');
-        });
-    });
-
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::prefix('super')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+    Route::prefix('units')->group(function () {
+        Route::post('/', [UnitsController::class, 'store'])->name('units.store');
+        Route::get('/', [UnitsController::class, 'index'])->name('units.index');
+        Route::get('/create', [UnitsController::class, 'create'])->name('units.create');
+        Route::delete('/{id}', [UnitsController::class, 'destroy'])->where('id', '[0-9]+')->name('units.destroy');
+        Route::get('/toggle/{id}', [UnitsController::class, 'toggleStatus'])->where('id', '[0-9]+')->name('units.toggleStatus');
+        Route::get('/{id}/edit', [UnitsController::class, 'edit'])->where('id', '[0-9]+')->name('units.edit');
+        Route::put('/{id}', [UnitsController::class, 'update'])->where('id', '[0-9]+')->name('units.update');
+        Route::get('/{id}/services', [UnitsServicesController::class, 'services'])->name('units.services');
+        Route::post('/{id}/services/store', [UnitsServicesController::class, 'storeServices'])->name('units.services.store');
+        Route::put('/{id}/services/save', [UnitsServicesController::class, 'saveServices'])->name('units.services.save');
+    });
+
+    Route::prefix('services')->group(function () {
+        Route::post('/', [ServiceController::class, 'store'])->name('services.store');
+        Route::get('/', [ServiceController::class, 'index'])->name('services.index');
+        Route::get('/create', [ServiceController::class, 'create'])->name('services.create');
+        Route::delete('/{id}', [ServiceController::class, 'destroy'])->where('id', '[0-9]+')->name('services.destroy');
+        Route::get('/toggle/{id}', [ServiceController::class, 'toggleStatus'])->where('id', '[0-9]+')->name('services.toggleStatus');
+        Route::get('/{id}/edit', [ServiceController::class, 'edit'])->where('id', '[0-9]+')->name('services.edit');
+        Route::put('/{id}', [ServiceController::class, 'update'])->where('id', '[0-9]+')->name('services.update');
+    });
+});
+
+Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 //rotas de agendamentos do user logado
 Route::prefix('schedules')->group(function () {
     Route::get('/', [SchedulesController::class, 'index'])->name('schedules.new');
