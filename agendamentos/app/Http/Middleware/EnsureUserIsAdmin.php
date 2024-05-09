e0375a1b2e8800746b90073a66a44f3f3effe82c
gabrielprogramadorweb <gabriel.programador.ads@gmail.com>
2024-05-09 17:57:06 -0300

    resolve erros

--- agendamentos/app/Http/Middleware/EnsureUserIsAdmin.php
+++ agendamentos/app/Http/Middleware/EnsureUserIsAdmin.php
@@ -4,10 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-<<<<<<< HEAD
-=======
 use Illuminate\Support\Facades\Auth;
->>>>>>> master
 
 class EnsureUserIsAdmin
 {
@@ -15,15 +12,6 @@ class EnsureUserIsAdmin
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-<<<<<<< HEAD
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
-    public function handle($request, Closure $next)
-    {
-        if (!auth()->check() || !auth()->user()->is_admin) {
-            return redirect('/')->with('error', 'Você não tem permissão para acessar essa página.');
-=======
      * @param  \Closure  $next
      * @return mixed
      */
@@ -31,13 +19,8 @@ class EnsureUserIsAdmin
     {
         if (!Auth::check() || !Auth::user()->is_admin) {
             return redirect('/');
->>>>>>> master
         }
 
         return $next($request);
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 }
